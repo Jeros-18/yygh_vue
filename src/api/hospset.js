f77f9e1a8d3b2cb776b49bfd89cf2a11735ad215
小星尘 <jahui6690429@163.com>
2021-07-11 19:57:37 +0800

    医院设置前端完成 p56

--- src/api/hospset.js
+++ src/api/hospset.js
@@ -8,5 +8,61 @@ export default{
       data: searchObj
 
     })
-  }
+  },
+
+  deleteHospSet(id){
+    return request({
+      url:`/admin/hosp/hospitalSet/${id}`,
+      method: 'delete'
+    })
+  },
+
+  // removeRows(idList) {
+  //   return request({
+  //   url: `${api_name}/batchRemove`,
+  //   method: 'delete',
+  //   data: idList
+  //     })
+  //   }
+  batchRemoveHospSet(idList){
+    return request({
+      url: '/admin/hosp/hospitalSet/batchRemove',
+      method: 'delete',
+      data: idList
+    })
+  },
+
+  //锁定和取消锁定
+  lockHospSet(id,status) {
+      return request ({
+        url: `/admin/hosp/hospitalSet/lockHospitalSet/${id}/${status}`,
+        method: 'put'
+      })
+    },
+
+    //添加医院设置
+  saveHospSet(hospitalSet) {
+      return request ({
+        url: `/admin/hosp/hospitalSet/saveHospitalSet`,
+        method: 'post',
+        data: hospitalSet
+      })
+    },
+
+    //院设置id查询
+  getHospSet(id) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/getHospSet/${id}`,
+      method: 'get'
+    })
+  },
+
+ //修改医院设置
+  updateHospSet(hospitalSet) {
+      return request ({
+        url: `/admin/hosp/hospitalSet/updateHospitalSet`,
+        method: 'post',
+        data: hospitalSet
+      })
+    }
 }
